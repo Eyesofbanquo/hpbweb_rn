e99e0d272416a9a9e2c964e776ffd2c83ff257ec
Markim <ms79723@gmail.com>
2021-03-12 14:10:32 -0600

    Create usBookImageService hook

---
+++ src/hooks/useBookImageService.tsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+interface BookImageServiceProps {
+  hpb: string;
+  alibris: string;
+}
+
+const BASE_URL = `https://aiief0969h.execute-api.us-east-1.amazonaws.com/default/isbn/`;
+
+export const useBookImageService = (upc: string) => {
+  const [response, setResponse] = useState<BookImageServiceProps>();
+  const [useFallback, setUseFallback] = useState<boolean>(false);
+
+  useEffect(() => {
+    axios.get(BASE_URL + upc).then((res) => {
+      setResponse(res.data as BookImageServiceProps);
+    });
+  }, [useFallback, upc]);
+
+  return {
+    response,
+    useFallbackUri: (condition: boolean) => {
+      setUseFallback(condition);
+    },
+  };
+};
