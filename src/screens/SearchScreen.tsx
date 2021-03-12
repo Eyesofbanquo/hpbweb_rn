6ddbac809e8f0cca5c7d7db39e229a85e41d111f
Markim <ms79723@gmail.com>
2021-03-12 03:20:29 -0600

    Add the ability to live search

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { FlatList, View } from 'react-native';
+import { FlatList, View, Text } from 'react-native';
 import { SearchBar } from 'react-native-elements';
 import FastImage from 'react-native-fast-image';
 
@@ -18,37 +18,66 @@ export type SearchScreenNavigationProp = StackNavigationProp<
 
 export type SearchScreenRouteProp = RouteProp<SearchStackParamList, 'Search'>;
 
+const filteredResponse = (predicate: string, response?: LiveSearch[]) => {
+  if (Array.isArray(response) === false) {
+    return [];
+  }
+  if (predicate.length === 0) {
+    return response;
+  }
+  return response.filter((book) => book.name.includes(predicate));
+};
+
 export const SearchScreen: React.FC<{
   navigation: SearchScreenNavigationProp;
   route: SearchScreenRouteProp;
 }> = () => {
-  const { response } = useNetwork<LiveSearch>({
+  const [searchText, setSearchText] = useState<string>('');
+  const {
+    response,
+    updateSearch: updateNetworkSearch,
+  } = useNetwork<LiveSearch>({
     endpoint: 'live',
-    options: { search: 'Christopher Moore' },
+    options: { search: searchText },
   });
+
+  const updateSearch = (search: string) => {
+    setSearchText(search);
+    updateNetworkSearch(search);
+  };
+
+  console.log(response, searchText);
 
   return (
     <View style={{ flex: 1, height: '100%' }}>
-      <SearchBar placeholder="Type Here..." platform="ios" />
-      <FlatList
-        data={response}
-        renderItem={({ item }) => {
-          return (
-            item.slug && (
-              <FastImage
-                style={{ width: '33%', height: 200 }}
-                source={{
-                  uri: imageUrlFactor(item.upc),
-                  priority: FastImage.priority.normal,
-                }}
-                resizeMode={FastImage.resizeMode.contain}
-              />
-            )
-          );
-        }}
-        numColumns={3}
-        keyExtractor={(item, index) => `${index}`}
+      <SearchBar
+        placeholder="Type Here..."
+        platform="ios"
+        value={searchText}
+        onChangeText={updateSearch}
       />
+      {response.length > 0 && (
+        <FlatList
+          data={response}
+          renderItem={({ item, index }) => {
+            return (
+              item.slug && (
+                <FastImage
+                  key={index}
+                  style={{ width: '33%', height: 200 }}
+                  source={{
+                    uri: imageUrlFactor(item.upc),
+                    priority: FastImage.priority.normal,
+                  }}
+                  resizeMode={FastImage.resizeMode.contain}
+                />
+              )
+            );
+          }}
+          numColumns={3}
+          keyExtractor={(item, index) => `${index}`}
+        />
+      )}
     </View>
   );
 };
