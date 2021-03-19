35a882e29e90a271b9dd21880dc7e3be64d8e2f5
Markim Shaw <ms79723@gmail.com>
2021-03-19 16:25:08 -0500

    Add remote config test
    - Add test code inside of `App.tsx` to test remote config

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
@@ -17,10 +17,36 @@ import { enableScreens } from 'react-native-screens';
 import { SearchStackScreen } from './src/navigation/search-stack';
 import { SettingsTab } from './src/navigation/settings-stack';
 import { MainTab } from './src/navigation/tab-stack';
+import remoteConfig from '@react-native-firebase/remote-config';
 
 enableScreens();
 
 const App = () => {
+  useEffect(() => {
+    remoteConfig()
+      .setConfigSettings({
+        minimumFetchIntervalMillis: 30,
+      })
+      .then(() => {
+        return remoteConfig().setDefaults({
+          aweseome_new_feature: 'disabled',
+        });
+      })
+      .then(() => remoteConfig().fetchAndActivate())
+      .then((fetchedRemotely) => {
+        if (fetchedRemotely) {
+          console.log('Configs were retrieved and activated');
+        } else {
+          console.log('NO configs were fetched');
+        }
+      });
+  }, []);
+  const awesomeNewFeature = remoteConfig().getValue('awesome_new_feature');
+
+  console.log(awesomeNewFeature.getSource());
+  if (awesomeNewFeature.asString() == 'enabled') {
+    console.log('this was activated');
+  }
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
