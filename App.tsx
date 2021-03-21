5df0b51519d2b8a851d74cd111b4f5033135bb8d
Markim Shaw <ms79723@gmail.com>
2021-03-20 19:08:00 -0500

    Add theme provider to app
    Add theme provider to top level of app. This uses a context

--- App.tsx
+++ App.tsx
@@ -14,11 +14,13 @@ import remoteConfig from '@react-native-firebase/remote-config';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
 import { enableScreens } from 'react-native-screens';
+import { ThemeProvider } from 'styled-components';
 
 import { useSetupConfig } from './src/config/useConfig';
 import { SearchStackScreen } from './src/navigation/search-stack';
 import { SettingsTab } from './src/navigation/settings-stack';
 import { MainTab } from './src/navigation/tab-stack';
+import { theme } from './src/ui/theme';
 
 enableScreens();
 
@@ -34,10 +36,12 @@ const App = () => {
   return (
     <NavigationContainer>
       <StatusBar barStyle="dark-content" />
-      <MainTab.Navigator>
-        <MainTab.Screen name="Search" component={SearchStackScreen} />
-        <MainTab.Screen name="Settings" component={SettingsTab} />
-      </MainTab.Navigator>
+      <ThemeProvider theme={theme}>
+        <MainTab.Navigator>
+          <MainTab.Screen name="Search" component={SearchStackScreen} />
+          <MainTab.Screen name="Settings" component={SettingsTab} />
+        </MainTab.Navigator>
+      </ThemeProvider>
     </NavigationContainer>
   );
 };
