5834335fb364b93df4d4f59723a3d105cc5bd8e0
Markim Shaw <ms79723@gmail.com>
2021-04-03 21:18:08 -0500

    Add initial files

--- App.tsx
+++ App.tsx
@@ -10,7 +10,13 @@
 
 import React from 'react';
 
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
+import { NormalSearch } from '@eyesofbanquo/hpbtypes';
 import remoteConfig from '@react-native-firebase/remote-config';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'react-native';
@@ -22,12 +28,79 @@ import { SearchStackScreen } from './src/navigation/search-stack';
 import { SettingsTab } from './src/navigation/settings-stack';
 import { MainTab } from './src/navigation/tab-stack';
 import { theme } from './src/ui/theme';
-
 enableScreens();
+
+const BOOK_Q = gql`
+  query BookQueryWrite {
+    book {
+      id
+      category
+    }
+  }
+`;
+
+const typeDefs = gql`
+  type Book {
+    id: String
+    category: String
+  }
+
+  type BookResult {
+    books: [Book]
+  }
+
+  extend type Query {
+    books: BookResult
+  }
+`;
 
 const client = new ApolloClient({
   uri: 'https://8chh8ip4l4.execute-api.us-east-1.amazonaws.com/dev/graphql',
-  cache: new InMemoryCache(),
+  connectToDevTools: true,
+  typeDefs: typeDefs,
+  cache: new InMemoryCache({
+    possibleTypes: {
+      Search: ['NormalSearch', 'TopSearch', 'LiveSearch'],
+    },
+    typePolicies: {
+      books: {},
+      BookResult: {
+        fields: {
+          books: {
+            read: (value, { cache }) => {},
+          },
+        },
+      },
+      Category: {
+        keyFields: ['id', 'name'],
+      },
+      NormalSearch: {
+        keyFields: ['id', 'slug'],
+        fields: {
+          id: {
+            merge: (existing: string, incoming: string, { cache }) => {
+              cache.writeFragment({
+                id: `BookResult:1`,
+                fragment: gql`
+                  fragment MyBook on Book {
+                    id
+                    category
+                  }
+                `,
+                data: {
+                  __typename: 'Book',
+                  id: incoming,
+                  category: 'none',
+                },
+              });
+              console.log('refetched');
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 const App = () => {
--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { useQuery, gql } from '@apollo/client';
+import { NormalSearch } from '@eyesofbanquo/hpbtypes';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import { View, Text } from 'react-native';
+import { View, Text, ActivityIndicator, Button } from 'react-native';
 
 import { SettingsStackParamList } from '../navigation/settings-stack';
 
@@ -14,10 +16,68 @@ type Props = {
   route: RouteProps;
 };
 
+const BOOKS_QUERY = gql`
+  query GetBooks {
+    books: normal(input: { search: "Christopher Moore" }) {
+      id
+      slug
+      __typename
+      gqlType
+      name
+      categories {
+        id
+        name
+      }
+    }
+  }
+`;
+
+interface NormalSearchResults {
+  books: [NormalSearch];
+}
+
 export const SettingsScreen: React.FC<Props> = () => {
+  const {
+    loading,
+    error,
+    data,
+    refetch,
+    networkStatus,
+  } = useQuery<NormalSearchResults>(BOOKS_QUERY, {
+    notifyOnNetworkStatusChange: true,
+  });
+
+  if (loading) {
+    return (
+      <View>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View>
+        <Text>Something went wrong</Text>
+      </View>
+    );
+  }
+
   return (
     <View>
-      <Text>This is the Settings Screen!</Text>
+      {data.books.map((book, index) => {
+        return (
+          <View key={index}>
+            <Text>{book.name}</Text>
+          </View>
+        );
+      })}
+      <Button
+        title="Refetch data"
+        onPress={() => {
+          refetch();
+        }}
+      />
     </View>
   );
 };
