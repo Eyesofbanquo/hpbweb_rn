519c8d7be1e4f130ecfcdc03128eb63b698e565d
Markim Shaw <djsalinger67@live.com>
2021-04-04 15:00:59 -0500

    Feature/apollo client (#14)
    * Add Apollo Client
    
    - Add Apollo Client dependency
    - Add ApolloProvider to App.tsx
    
    * Add initial files
    
    * Finalize apollo client hookup
    
    Co-authored-by: Markim Shaw <ms79723@gmail.com>

--- App.tsx
+++ App.tsx
@@ -10,8 +10,15 @@
 
 import React from 'react';
 
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from '@apollo/client';
 import remoteConfig from '@react-native-firebase/remote-config';
 import { NavigationContainer } from '@react-navigation/native';
+import { values } from 'lodash';
 import { StatusBar } from 'react-native';
 import { enableScreens } from 'react-native-screens';
 import { ThemeProvider } from 'styled-components/native';
@@ -21,8 +28,134 @@ import { SearchStackScreen } from './src/navigation/search-stack';
 import { SettingsTab } from './src/navigation/settings-stack';
 import { MainTab } from './src/navigation/tab-stack';
 import { theme } from './src/ui/theme';
-
 enableScreens();
+
+const BOOK_Q = gql`
+  query BookQueryWrite {
+    book {
+      id
+      category
+    }
+  }
+`;
+
+const typeDefs = gql`
+  extend type Book {
+    id: String
+    category: String
+  }
+
+  extend type BookResult {
+    id: String
+    books: [Book]
+  }
+
+  extend type Query {
+    bookResults: BookResult
+    books: [Book]
+  }
+`;
+
+const client = new ApolloClient({
+  uri: 'https://8chh8ip4l4.execute-api.us-east-1.amazonaws.com/dev/graphql',
+  connectToDevTools: true,
+  typeDefs: typeDefs,
+  cache: new InMemoryCache({
+    possibleTypes: {
+      Search: ['NormalSearch', 'TopSearch', 'LiveSearch'],
+    },
+    typePolicies: {
+      Book: {
+        keyFields: ['id', 'category'],
+        fields: {
+          id: {
+            merge: true,
+          },
+        },
+      },
+      books: {
+        fields: {
+          books: {
+            merge: (existing, incoming) => {
+              console.log('ok');
+
+              return incoming;
+            },
+          },
+        },
+      },
+      BookResult: {
+        fields: {
+          books: {
+            read: (value, { cache }) => {
+              console.log(value, 'book result read');
+              return value;
+            },
+            merge: (existing = [], incoming, { variables }) => {
+              console.log(incoming, 'book result merge');
+
+              return [...existing, ...incoming];
+            },
+          },
+        },
+      },
+      Category: {
+        keyFields: ['id', 'name'],
+      },
+      NormalSearch: {
+        keyFields: ['id', 'slug'],
+        fields: {
+          id: {
+            merge: (existing: string, incoming: string, { cache }) => {
+              const { books } = {
+                ...cache.readQuery({
+                  query: gql`
+                    query BookResult {
+                      books {
+                        id
+                        category
+                      }
+                    }
+                  `,
+                }),
+              };
+              cache.writeQuery({
+                query: gql`
+                  query BookResult {
+                    books {
+                      id
+                      category
+                    }
+                  }
+                `,
+                data: {
+                  books:
+                    books !== undefined
+                      ? [
+                          ...books,
+                          {
+                            __typename: 'Book',
+                            id: incoming,
+                            category: 'none',
+                          },
+                        ]
+                      : [
+                          {
+                            __typename: 'Book',
+                            id: incoming,
+                            category: 'none',
+                          },
+                        ],
+                },
+              });
+              return incoming;
+            },
+          },
+        },
+      },
+    },
+  }),
+});
 
 const App = () => {
   useSetupConfig();
@@ -34,15 +167,17 @@ const App = () => {
     console.log('this was activated');
   }
   return (
-    <NavigationContainer>
-      <StatusBar barStyle="dark-content" />
-      <ThemeProvider theme={theme}>
-        <MainTab.Navigator>
-          <MainTab.Screen name="Search" component={SearchStackScreen} />
-          <MainTab.Screen name="Settings" component={SettingsTab} />
-        </MainTab.Navigator>
-      </ThemeProvider>
-    </NavigationContainer>
+    <ApolloProvider client={client}>
+      <NavigationContainer>
+        <StatusBar barStyle="dark-content" />
+        <ThemeProvider theme={theme}>
+          <MainTab.Navigator>
+            <MainTab.Screen name="Search" component={SearchStackScreen} />
+            <MainTab.Screen name="Settings" component={SettingsTab} />
+          </MainTab.Navigator>
+        </ThemeProvider>
+      </NavigationContainer>
+    </ApolloProvider>
   );
 };
 
